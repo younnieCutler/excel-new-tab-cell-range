8af1b322cb923f171cc15931d2d0aa6785f84ec8
younnieCutler <younnieCutler@users.noreply.github.com>
2026-05-13 06:02:49 +0000

    deploy: d5f09dbccc0217893af8f4d6fb33defe906aba1b

---
+++ commands.bundle.js
@@ -0,0 +1,1 @@
+(()=>{function e(e){Office.addin.showAsTaskpane().then(function(){"function"==typeof window.captureSelectedRange&&window.captureSelectedRange()}),e.completed()}Office.onReady(function(){Office.actions.associate("openInCellFocus",e)})})();
